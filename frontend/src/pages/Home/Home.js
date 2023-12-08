bb70c1b0d97bf8fe84c3301a29ea473fd47aba57
LecinoLucas <lecinolucas5@gmail.com>
2023-12-08 10:00:28 -0300

    Projeto Finalizado

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -54,7 +54,8 @@ const Home = () => {
      // {photos && photos.length === 0 && (
         <h2 className="no-photos">
           Ainda não há fotos publicadas,{" "}
-          <Link to={`/users/${user.userId}`}>clique aqui</Link> para começar.
+          
+          <Link to={`/users/${user._id}`}>clique aqui</Link> para começar.
         </h2>
       )}
     </div>
